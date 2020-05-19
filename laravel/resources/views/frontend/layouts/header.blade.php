a51e6a62019ef0fbca7829aae4c4a72f8de5a57b
Muhammad Febriansyah <febriansyah.7@gmail.com>
2020-05-19 22:34:14 +0700

    add gtm

--- laravel/resources/views/frontend/layouts/header.blade.php
+++ laravel/resources/views/frontend/layouts/header.blade.php
@@ -1,15 +1,13 @@
 <!doctype html>
 <html>
 <head>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-166080091-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-166080091-1');
-</script>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MN4XF8P');</script>
+<!-- End Google Tag Manager -->
 
 <meta charset="UTF-8">
 <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -285,3 +283,7 @@ function setVideoInfo( videoUrl, videoThumb){
 </script>
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MN4XF8P"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
